a770d4bfed0288ba386109774529346f8935b114
Yusuf Taha Özlü <yozlu@k1m37s01.42istanbul.com.tr>
2025-05-16 17:44:10 +0300

    exit basitçe bitti

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 
 #include "minishell.h"
 
+int exit_time(char *input)
+{
+	int returnnumber;
+	char *tmp;
+
+	returnnumber = 0;
+	if (input[4])
+	{
+		tmp = ft_substr(input, 5, ft_strlen(input) - 5);
+		returnnumber = ft_atoi(tmp);
+		returnnumber = returnnumber % 256;
+	}
+	return (returnnumber);
+}
+
 int main(int ac, char **av)
 {
 	char *input;
@@ -17,9 +32,9 @@ int main(int ac, char **av)
 	{
 		input = readline(name);
 		add_history(input);
-		if (!ft_strncmp(input, "exit", 4))
-			break; //exit_time();
-		printf("%s\n", input);
+		if (!ft_strncmp(input, "exit", 4) && (input[4] == 32 || !input[4]))
+			return (exit_time(input));
+		// parser();
 	}
 	free(input);
 	free(tmp1);
