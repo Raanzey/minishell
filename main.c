0b869c93b2fb3b289e1413063c624025b34ce9c6
Mustafa Ersin Şişman - WSL <sismanmustafaersin@gmail.com>
2025-05-17 20:09:32 +0300

    parser için struct oluşturuldu ve tokenizer işlemine başllanacak

--- main.c
+++ main.c
@@ -56,26 +56,18 @@ int exit_time(char *input)
 int main(int ac, char **av)
 {
 	char *input;
-	char *tmp1;
-	char *tmp2;
-	char *name;
 	
+	(void)av;
 	if (ac >= 2)
 		return (error(ERR_ARG));
-	tmp1 = ft_substr(av[0], 2, 9);
-	tmp2 = ft_strjoin(tmp1, "~ ");
-	name = ft_strdup(tmp2);
 	while (1)
 	{
-		input = readline(name);
+		input = readline("minishell~ ");
 		add_history(input);
 		if (!ft_strncmp(input, "exit", 4) && (input[4] == 32 || !input[4]))
-			return (exit_time(input));
-		// parser();
+			return (exit_time(input)); // TODO exit built in burası silinecek
+		parser(input);
 	}
 	free(input);
-	free(tmp1);
-	free(tmp2);
-	free(name);
 	return (0);
 }
